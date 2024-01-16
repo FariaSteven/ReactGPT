6a79e429666c209a1da826cdd0d73be82c88bee8
Steven Faria <stevenfaria.gusmao@gmail.com>
2024-01-16 12:07:40 -0300

    Adding speech recognition and bard link

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,70 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 import './App.css';
-import { VStack } from '@chakra-ui/react';
+import { Button, Input, VStack } from '@chakra-ui/react';
 
 const App = () => {
   const [bardReturn, setBardReturn] = useState<string>();
+  const [speechResult, setSpeechResult] = useState<string>("");
 
-  const promptReturn = async () => {
+  const SpeechToText = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  if (!SpeechToText) {
+    console.error('SpeechRecognition is not available in this browser.');
+  }
+
+  const speech = new SpeechToText();
+  speech.continuous = true;
+  speech.lang = 'pt-BR';
+
+  speech.onresult = (e) => {
+    let resultIndex = e.resultIndex;
+    let transcript = e.results[resultIndex][0].transcript;
+
+    setSpeechResult(transcript);
+  };
+
+  const start = () => {
+    speech.start();
+  };
+
+  const stop = () => {
+    speech.stop();
+  };
+
+  const promptReturn = useCallback(async() => {
     const genAI = new GoogleGenerativeAI(import.meta.env.VITE_REACT_APP_API_KEY);
     const generationConfig = {
-      stopSequences: ["red"],
+      stopSequences: ['red'],
       maxOutputTokens: 200,
       temperature: 0.9,
       topP: 0.1,
       topK: 16,
     };
-    const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig })
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro', generationConfig });
 
-    const prompt = "√49";
-
-    const result = await model.generateContent(prompt);
+    const prompt = speechResult;
+    const result =  await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    setBardReturn(text)
+    setBardReturn(text);
     console.log(text);
-  };
+  }, [speechResult])
 
   useEffect(() => {
-    promptReturn()
-  }, [])
+    promptReturn();
+  }, [speechResult]);
 
   return (
-    <VStack  color="#fff" >
-        <p>asdasd</p>
-        <p>{bardReturn}</p>
+    <VStack color="#fff">
+      <p>asdasd</p>
+      <p>{bardReturn}</p>
+      <Input defaultValue={speechResult} />
+      <Button onClick={start}>Gravar</Button>
+      <Button onClick={stop}>Parar</Button>
     </VStack>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/BardReturn/BardReturn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BardReturn = () => {
+  return (
+    <div>BardReturn</div>
+  )
+}
+
+export default BardReturn
